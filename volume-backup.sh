6c22c56b53492e69c0e0ef3cede0afbc6c423d6f
Ariel Ouziel <ariel.ouziel.external@atos.net>
2020-07-02 16:10:15 +0200

    remove error when volume is empty

--- volume-backup.sh
+++ volume-backup.sh
@@ -10,10 +10,10 @@ usage() {
 }
 
 backup() {
-    if [ -z "$(ls -A /volume)" ]; then
-       >&2 echo "Volume is empty or missing, check if you specified a correct name"
-       exit 1
-    fi
+#    if [ -z "$(ls -A /volume)" ]; then
+#       >&2 echo "Volume is empty or missing, check if you specified a correct name"
+#       exit 1
+#    fi
 
     if ! [ "$ARCHIVE" == "-" ]; then
         mkdir -p `dirname /backup/$ARCHIVE`
